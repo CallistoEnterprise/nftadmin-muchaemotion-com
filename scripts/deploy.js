37a2ba71b74af0af6b5f939c379ec94c0bfb32e9
Erik <esnetto@gmail.com>
2022-11-01 22:21:50 +0000

    WIP: first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+// How to: npx hardhat run scripts/deploy.js --network <network-name>
+
+async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  console.log('Deploying contracts with the account:', deployer.address)
+
+  console.log('Account balance:', (await deployer.getBalance()).toString())
+
+  const Contract = await ethers.getContractFactory('MuchaNFT')
+  const contract = await Contract.deploy()
+
+  console.log('Contract address:', contract.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
